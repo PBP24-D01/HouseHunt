350510ba682d64e14d4468b5e4e526274197ca71
Tristan A. <tristanagraa@gmail.com>
2024-10-23 17:21:07 +0700

    Refactor: module name and add HouseHuntAuth to installed apps

--- HouseHunt/settings.py
+++ HouseHunt/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = ["*", "http://tristan-agra-househunt.pbp.cs.ui.ac.id"]
 AUTH_USER_MODEL = 'HouseHuntAuth.CustomUser'
 
 INSTALLED_APPS = [
+    "HouseHuntAuth",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "HouseHuntAuth",
 ]
 
 MIDDLEWARE = [
