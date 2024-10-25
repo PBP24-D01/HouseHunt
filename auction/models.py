57234654bdee9146efcc679ba1f3e51d6244b811
Tristan A. <tristanagraa@gmail.com>
2024-10-25 21:26:36 +0700

    feat: auction models

--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from HouseHuntAuth.models import Buyer, Seller
+from rumah.models import House
 # Create your models here.
+
+
+class Auction(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
