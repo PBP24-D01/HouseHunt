03519d9cd34a2644edd9a0b3b567d7825b5518e4
Tristan A. <tristanagraa@gmail.com>
2024-10-27 00:02:20 +0700

    Refactor: Add trusted origins for CSRF protection

--- HouseHunt/settings.py
+++ HouseHunt/settings.py
@@ -29,6 +29,10 @@ DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["*", "http://tristan-agra-househunt.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+]
 
 # Application definition
 
