24099f590609721766b5c8da63c8de318a42221f
Tristan A. <tristanagraa@gmail.com>
2024-12-03 20:19:12 +0700

    fix: register

--- HouseHuntAuth/urls.py
+++ HouseHuntAuth/urls.py
@@ -2,15 +2,13 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-app_name = "HouseHuntAuth"
-
 urlpatterns = [
     path("register/buyer/", views.RegisterBuyer.as_view(), name="buyer_signup"),
     path("register/seller/", views.RegisterSeller.as_view(), name="seller_signup"),
     path("login/", views.login_user, name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("profile/", views.profile, name="profile"),
-    path("login/flutter/", views.login, name="login_flutter"),
+    path("login/flutter/", views.login_api, name="login_flutter"),
     path("register/buyer/flutter/", views.register_buyer, name="buyer_signup_flutter"),
     path("register/seller/flutter/", views.register_seller, name="seller_signup_flutter"),
     path("logout/flutter/", views.logout, name="logout_flutter"),
--- HouseHuntAuth/views.py
+++ HouseHuntAuth/views.py
@@ -3,9 +3,9 @@ import json
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import login
 from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
-from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView
 from .forms import BuyerSignUpForm, SellerSignUpForm
@@ -66,33 +66,41 @@ def login_user(request):
     context = {"form": form}
     return render(request, "login.html", context)
 
+
 @csrf_exempt
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
+def login_api(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
             auth_login(request, user)
             # Status login sukses.
-            return JsonResponse({
-                "id": user.id,
-                "username": user.username,
-                "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
-            }, status=200)
+            return JsonResponse(
+                {
+                    "id": user.id,
+                    "username": user.username,
+                    "status": True,
+                    "message": "Login sukses!",
+                    # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
+                },
+                status=200,
+            )
         else:
-            return JsonResponse({
-                "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
-            }, status=401)
+            return JsonResponse(
+                {"status": False, "message": "Login gagal, akun dinonaktifkan."},
+                status=401,
+            )
 
     else:
-        return JsonResponse({
-            "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
-        }, status=401)
+        return JsonResponse(
+            {
+                "status": False,
+                "message": "Login gagal, periksa kembali email atau kata sandi.",
+            },
+            status=401,
+        )
+
 
 @csrf_exempt
 def register_seller(request):
@@ -154,7 +162,7 @@ def register_seller(request):
 
         return JsonResponse(
             {
-                "username": user.get_full_name(),
+                "username": username,
                 "status": "success",
                 "message": "User created successfully!",
             },
@@ -225,7 +233,7 @@ def register_buyer(request):
 
         return JsonResponse(
             {
-                "username": user.get_full_name(),
+                "username": username,
                 "status": "success",
                 "message": "User created successfully!",
             },
@@ -237,33 +245,6 @@ def register_buyer(request):
             {"status": False, "message": "Invalid request method."}, status=400
         )
 
-@csrf_exempt
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            auth_login(request, user)
-            # Status login sukses.
-            return JsonResponse({
-                "id": user.id,
-                "username": user.username,
-                "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
-            }, status=200)
-        else:
-            return JsonResponse({
-                "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
-            }, status=401)
-
-    else:
-        return JsonResponse({
-            "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
-        }, status=401)
 
 @csrf_exempt
 def logout(request):
@@ -271,13 +252,9 @@ def logout(request):
 
     try:
         auth_logout(request)
-        return JsonResponse({
-            "username": username,
-            "status": True,
-            "message": "Logout berhasil!"
-        }, status=200)
+        return JsonResponse(
+            {"username": username, "status": True, "message": "Logout berhasil!"},
+            status=200,
+        )
     except:
-        return JsonResponse({
-        "status": False,
-        "message": "Logout gagal."
-        }, status=401)
+        return JsonResponse({"status": False, "message": "Logout gagal."}, status=401)
