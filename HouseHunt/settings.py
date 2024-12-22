b97ae6927f6da8ac2fa840a2e1b809bd977b61e2
Tristan A. <tristanagraa@gmail.com>
2024-12-22 18:57:18 +0700

    chore: fix csrf on local

--- HouseHunt/settings.py
+++ HouseHunt/settings.py
@@ -36,12 +36,14 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "*"
     "http://localhost",
     "http://127.0.0.1",
     "http://tristan-agra-househunt.pbp.cs.ui.ac.id",
     "https://tristan-agra-househunt.pbp.cs.ui.ac.id",
     'http://localhost:54469',
     'http://127.0.0.1:54469',
+    'http://localhost:59335/',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
